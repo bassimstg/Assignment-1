8c15c81189c691013a5fb9b1a24888cfd70ce401
bassim.inmca2126 <bassim.inmca2126@OPENSOURCELAB37>
2024-05-16 10:13:42 +0530

    Add project files.

---
+++ student details/Module1.vb
@@ -0,0 +1,79 @@
+﻿Public Class Student
+    Public Property Id As Integer
+    Public Property Name As String
+    Public Property Age As Integer
+End Class
+
+Public Class StudentManager
+    Private students As New List(Of Student)()
+
+    Public Sub AddOrUpdateStudent(ByVal id As Integer)
+        Dim student = students.FirstOrDefault(Function(s) s.Id = id)
+        If student Is Nothing Then
+            student = New Student()
+            students.Add(student)
+            student.Id = id
+            Console.WriteLine("New student entry created.")
+        End If
+
+        'Inputting Student details
+        Console.Write("Enter student name: ")
+        student.Name = Console.ReadLine()
+
+        Console.Write("Enter student age: ")
+        student.Age = Convert.ToInt32(Console.ReadLine())
+
+        Console.WriteLine("Student record updated.")
+    End Sub
+
+    'Deleting Student details
+    Public Sub DeleteStudent(ByVal id As Integer)
+        Dim student = students.FirstOrDefault(Function(s) s.Id = id)
+        If student IsNot Nothing Then
+            students.Remove(student)
+            Console.WriteLine("Student record deleted.")
+        Else
+            Console.WriteLine("Student not found.")
+        End If
+    End Sub
+
+    Public Sub DisplayStudents()
+        For Each student In students
+            Console.WriteLine($"ID: {student.Id}, Name: {student.Name}, Age: {student.Age}")
+        Next
+    End Sub
+End Class
+
+
+'Giving choices whether the user wants to insert update Or delete the student details
+Module Module1
+    Sub Main()
+        Dim manager As New StudentManager()
+        Dim choice As Integer
+        Do
+            Console.WriteLine("1. Add/Update Student")
+            Console.WriteLine("2. Delete Student")
+            Console.WriteLine("3. Display Students")
+            Console.WriteLine("4. Exit")
+            Console.Write("Select an option: ")
+            choice = Convert.ToInt32(Console.ReadLine())
+
+            Select Case choice
+                Case 1
+                    Console.Write("Enter student ID: ")
+                    Dim id As Integer = Convert.ToInt32(Console.ReadLine())
+                    manager.AddOrUpdateStudent(id)
+                Case 2
+                    Console.Write("Enter student ID to delete: ")
+                    Dim id As Integer = Convert.ToInt32(Console.ReadLine())
+                    manager.DeleteStudent(id)
+                Case 3
+                    manager.DisplayStudents()
+                Case 4
+                    Exit Do
+                Case Else
+                    Console.WriteLine("Invalid option, please try again.")
+            End Select
+        Loop While choice <> 4
+    End Sub
+End Module
---
+++ student details/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("student details")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("student details")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("121986fd-74a2-410f-b94c-8657839672f7")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
